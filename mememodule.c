1bbdac3ddc3a6adc865ffc90e6c3e72b88aa57e2
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-13 01:32:53 +0800

    Update mememodule.c

--- mememodule.c
+++ mememodule.c
@@ -1,24 +1,58 @@
-from distutils.core import setup, Extension
+#include <Python.h>
 
-# meme_hash_module
-meme_hash_module = Extension('meme_hash',  
-                             sources=['mememodule.c',  
-                                      'memehash.c',
-                                      'sha3/blake.c',
-                                      'sha3/bmw.c',
-                                      'sha3/groestl.c',
-                                      'sha3/jh.c',
-                                      'sha3/keccak.c',
-                                      'sha3/skein.c',
-                                      'sha3/cubehash.c',
-                                      'sha3/echo.c',
-                                      'sha3/luffa.c',
-                                      'sha3/simd.c',
-                                      'sha3/shavite.c'],
-                             include_dirs=['.', './sha3'])
+#include "memehash.h"  // memehash.h
 
-# 修改 setup 設定
-setup(name='meme_hash',  # 修改名稱
-      version='1.0',  # 可以更新版本號
-      description='Binding for Meme proof of work hashing.',
-      ext_modules=[meme_hash_module])
+static PyObject *meme_getpowhash(PyObject *self, PyObject *args) {
+    char *output;
+    PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
+    PyStringObject *input;
+#endif
+    if (!PyArg_ParseTuple(args, "S", &input))
+        return NULL;
+
+    Py_INCREF(input);
+    output = PyMem_Malloc(32);
+
+#if PY_MAJOR_VERSION >= 3
+    meme_hash((char *)PyBytes_AsString((PyObject*) input), output);  //  meme_hash
+#else
+    meme_hash((char *)PyString_AsString((PyObject*) input), output);  //  meme_hash 
+#endif
+
+    Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
+    value = Py_BuildValue("s#", output, 32);
+#endif
+    PyMem_Free(output);
+    return value;
+}
+
+static PyMethodDef MemeMethods[] = {
+    { "getPoWHash", meme_getpowhash, METH_VARARGS, "Returns the proof of work hash using meme hash" },
+    { NULL, NULL, 0, NULL }
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef MemeModule = {
+    PyModuleDef_HEAD_INIT,
+    "meme_hash",
+    "...",
+    -1,
+    MemeMethods
+};
+
+PyMODINIT_FUNC PyInit_meme_hash(void) {
+    return PyModule_Create(&MemeModule);
+}
+
+#else
+
+PyMODINIT_FUNC initmeme_hash(void) {
+    (void) Py_InitModule("meme_hash", MemeMethods);
+}
+#endif
