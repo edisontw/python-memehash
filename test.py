894d87d85c20b9a3bb8a8994274de47ee564f38e
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-16 22:50:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,31 @@
 import meme_hash
 from binascii import unhexlify, hexlify
-
 import unittest
 
-# 原先的 Dash block #1 參數不需要改變，除非你有新的區塊測試
-header_hex = ("02000000" +
-    "b67a40f3cd5804437a108f105533739c37e6229bc1adcab385140b59fd0f0000" +
-    "a71c1aade44bf8425bec0deb611c20b16da3442818ef20489ca1e2512be43eef"
-    "814cdb52" +
-    "f0ff0f1e" +
-    "dbf70100")
+# Block 1 header information
+header_hex = (
+    "20000000" +  # Version
+    "00000a308cc3b469703a3bc1aa55bc251a71c9287d7b413242592c0ab0a31f13" +  # Previous block hash
+    "3f7ab2aa69fd0ea038de256393455890e45cba83ca0f76fc506c416c96109014" +  # Merkle root hash
+    "646da2eb" +  # Timestamp
+    "1e0fffff" +  # Difficulty target
+    "05e205"  # Nonce
+)
 
-# 這裡需要更新為你計算出的 Meme hash 的期望值
-best_hash = 'your_calculated_meme_hash_result_here'  # 更新為正確的 hash 值
+# Known correct hash value for block 1
+best_hash = "00000add89b915d985d20a9b8983a3fb3a96516733f4d032f4e4c9da1e7d6223"
 
-class TestSequenceFunctions(unittest.TestCase):
+class TestMemeHash(unittest.TestCase):
 
     def setUp(self):
+        # Convert the header information from hex string to bytes
         self.block_header = unhexlify(header_hex)
         self.best_hash = best_hash
 
     def test_meme_hash(self):
+        # Use meme_hash to calculate the hash value of the block header
         self.pow_hash = hexlify(meme_hash.getPoWHash(self.block_header)).decode('utf-8')
+        # Verify that the calculated hash matches the known correct hash
         self.assertEqual(self.pow_hash, self.best_hash)
 
 if __name__ == '__main__':
