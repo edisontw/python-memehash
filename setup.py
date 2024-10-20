309c38e6d27d338fcaffe578d040a9bbd4f17a06
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-20 16:57:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ meme_hash_module = Extension('meme_hash',
                                       'sha3/echo.c',
                                       'sha3/luffa.c',
                                       'sha3/simd.c',
-                                      'sha3/shavite.c'],
+                                      'sha3/shavite.c',
+                                      'sha3/sph_sha2.c',],
                              include_dirs=['.', './sha3'])
 
 setup(name='meme_hash',
