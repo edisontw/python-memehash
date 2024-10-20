a664199f6bfb1432c8225d869d4b75bf6c60f0da
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-21 02:15:28 +0800

    Update memehash.c

--- memehash.c
+++ memehash.c
@@ -6,10 +6,9 @@
 #include "sha3/sph_shavite.h"
 #include "sha3/sph_simd.h"
 #include "sha3/sph_echo.h"
-#include "sha3/sha256.h"
+#include "sha3/sph_sha256.h"
 
 void meme_hash(const char* input, char* output, uint32_t len) {
-    // Define contexts for each hash algorithm
     sph_blake512_context     ctx_blake;
     sph_cubehash512_context  ctx_cubehash;
     sph_shavite512_context   ctx_shavite;
@@ -17,49 +16,40 @@ void meme_hash(const char* input, char* output, uint32_t len) {
     sph_echo512_context      ctx_echo;
     sph_sha256_context       ctx_sha256;
 
-    uint32_t hash[16];   // Intermediate hash result
-    uint32_t hashA[16];  // Final hash result
+    uint32_t hash[16];
+    uint32_t hashA[16];
 
-    // Blake hash computation
     sph_blake512_init(&ctx_blake);
     sph_blake512(&ctx_blake, input, len);
     sph_blake512_close(&ctx_blake, hash);
 
-    // SIMD hash computation
     sph_simd512_init(&ctx_simd);
     sph_simd512(&ctx_simd, hash, 64);
     sph_simd512_close(&ctx_simd, hash);
 
-    // Echo hash computation
     sph_echo512_init(&ctx_echo);
     sph_echo512(&ctx_echo, hash, 64);
     sph_echo512_close(&ctx_echo, hash);
 
-    // Cubehash hash computation
     sph_cubehash512_init(&ctx_cubehash);
     sph_cubehash512(&ctx_cubehash, hash, 64);
     sph_cubehash512_close(&ctx_cubehash, hash);
 
-    // Shavite hash computation
     sph_shavite512_init(&ctx_shavite);
     sph_shavite512(&ctx_shavite, hash, 64);
     sph_shavite512_close(&ctx_shavite, hash);
 
-    // First SHA256 hash computation
     sph_sha256_init(&ctx_sha256);
     sph_sha256(&ctx_sha256, hash, 64);
     sph_sha256_close(&ctx_sha256, hashA);
 
-    // Second SHA256 hash computation
     sph_sha256_init(&ctx_sha256);
-    sph_sha256(&ctx_sha256, hashA, 64);
+    sph_sha256(&ctx_sha256, hashA, 32);
     sph_sha256_close(&ctx_sha256, hashA);
 
-    // Third SHA256 hash computation
     sph_sha256_init(&ctx_sha256);
-    sph_sha256(&ctx_sha256, hashA, 64);
+    sph_sha256(&ctx_sha256, hashA, 32);
     sph_sha256_close(&ctx_sha256, hash);
 
-    // Copy the final hash result to the output buffer
     memcpy(output, hash, 32);
 }
