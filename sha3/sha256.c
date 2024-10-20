491a487dd3b8c8dc53b76e0550a5da254a0e1dde
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-21 02:09:30 +0800

    Update sha256.c

--- sha3/sha256.c
+++ sha3/sha256.c
@@ -1,6 +1,6 @@
 /*********************************************************************
 * Filename:   sha256.c
-* Author:     Brad Conte (brad AT bradconte.com)
+* Original Author:     Brad Conte (brad AT bradconte.com)
 * Copyright:
 * Disclaimer: This code is presented "as is" without any guarantees.
 * Details:    Implementation of the SHA-256 hashing algorithm.
@@ -11,148 +11,180 @@
                * http://csrc.nist.gov/publications/fips/fips180-2/fips180-2withchangenotice.pdf
               This implementation uses little endian byte order.
 *********************************************************************/
+#include "sph_sha256.h"
+#include <string.h>
 
-/*************************** HEADER FILES ***************************/
-#include <stdlib.h>
-#include <memory.h>
-#include "sha256.h"
+#define CH(X, Y, Z)    ((X & Y) ^ (~X & Z))
+#define MAJ(X, Y, Z)   ((X & Y) ^ (X & Z) ^ (Y & Z))
+#define ROTR(x, n)     (((x) >> (n)) | ((x) << (32 - (n))))
+#define SHR(x, n)      ((x) >> (n))
+#define S0(x)          (ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22))
+#define S1(x)          (ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25))
+#define s0(x)          (ROTR(x, 7) ^ ROTR(x, 18) ^ SHR(x, 3))
+#define s1(x)          (ROTR(x, 17) ^ ROTR(x, 19) ^ SHR(x, 10))
 
-/****************************** MACROS ******************************/
-#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
-#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
-
-#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
-#define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
-#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
-#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
-
-/**************************** VARIABLES *****************************/
-static const WORD k[64] = {
-	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
-	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
-	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
-	0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
-	0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
-	0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
-	0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
-	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+static const sph_u32 K[64] = {
+    // ... (same as before)
 };
 
-/*********************** FUNCTION DEFINITIONS ***********************/
-void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
+static void
+sha256_round(const sph_u32 *data, sph_u32 *val)
 {
-	WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+    sph_u32 T1, T2, A, B, C, D, E, F, G, H;
+    sph_u32 W[64];
+    int i;
 
-	for (i = 0, j = 0; i < 16; ++i, j += 4)
-		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
-	for ( ; i < 64; ++i)
-		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
-
-	a = ctx->state[0];
-	b = ctx->state[1];
-	c = ctx->state[2];
-	d = ctx->state[3];
-	e = ctx->state[4];
-	f = ctx->state[5];
-	g = ctx->state[6];
-	h = ctx->state[7];
-
-	for (i = 0; i < 64; ++i) {
-		t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
-		t2 = EP0(a) + MAJ(a,b,c);
-		h = g;
-		g = f;
-		f = e;
-		e = d + t1;
-		d = c;
-		c = b;
-		b = a;
-		a = t1 + t2;
-	}
-
-	ctx->state[0] += a;
-	ctx->state[1] += b;
-	ctx->state[2] += c;
-	ctx->state[3] += d;
-	ctx->state[4] += e;
-	ctx->state[5] += f;
-	ctx->state[6] += g;
-	ctx->state[7] += h;
+    for (i = 0; i < 16; i++)
+        W[i] = sph_dec32be(data + i);
+    for (i = 16; i < 64; i++)
+        W[i] = s1(W[i - 2]) + W[i - 7] + s0(W[i - 15]) + W[i - 16];
+    A = val[0];
+    B = val[1];
+    C = val[2];
+    D = val[3];
+    E = val[4];
+    F = val[5];
+    G = val[6];
+    H = val[7];
+    for (i = 0; i < 64; i++) {
+        T1 = H + S1(E) + CH(E, F, G) + K[i] + W[i];
+        T2 = S0(A) + MAJ(A, B, C);
+        H = G;
+        G = F;
+        F = E;
+        E = D + T1;
+        D = C;
+        C = B;
+        B = A;
+        A = T1 + T2;
+    }
+    val[0] += A;
+    val[1] += B;
+    val[2] += C;
+    val[3] += D;
+    val[4] += E;
+    val[5] += F;
+    val[6] += G;
+    val[7] += H;
 }
 
-void sha256_init(SHA256_CTX *ctx)
+void
+sph_sha256_init(sph_sha256_context *cc)
 {
-	ctx->datalen = 0;
-	ctx->bitlen = 0;
-	ctx->state[0] = 0x6a09e667;
-	ctx->state[1] = 0xbb67ae85;
-	ctx->state[2] = 0x3c6ef372;
-	ctx->state[3] = 0xa54ff53a;
-	ctx->state[4] = 0x510e527f;
-	ctx->state[5] = 0x9b05688c;
-	ctx->state[6] = 0x1f83d9ab;
-	ctx->state[7] = 0x5be0cd19;
+    cc->count = 0;
+    cc->state[0] = 0x6A09E667;
+    cc->state[1] = 0xBB67AE85;
+    cc->state[2] = 0x3C6EF372;
+    cc->state[3] = 0xA54FF53A;
+    cc->state[4] = 0x510E527F;
+    cc->state[5] = 0x9B05688C;
+    cc->state[6] = 0x1F83D9AB;
+    cc->state[7] = 0x5BE0CD19;
+    cc->ptr = 0;
 }
 
-void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len)
+void
+sph_sha256(sph_sha256_context *cc, const void *data, size_t len)
 {
-	WORD i;
+    size_t current;
+    unsigned char *buf;
 
-	for (i = 0; i < len; ++i) {
-		ctx->data[ctx->datalen] = data[i];
-		ctx->datalen++;
-		if (ctx->datalen == 64) {
-			sha256_transform(ctx, ctx->data);
-			ctx->bitlen += 512;
-			ctx->datalen = 0;
-		}
-	}
+    buf = cc->buf;
+    current = cc->ptr;
+    while (len > 0) {
+        size_t clen;
+
+        clen = 64 - current;
+        if (clen > len)
+            clen = len;
+        memcpy(buf + current, data, clen);
+        data = (const unsigned char *)data + clen;
+        current += clen;
+        len -= clen;
+        if (current == 64) {
+            sha256_round((sph_u32 *)buf, cc->state);
+            current = 0;
+        }
+    }
+    cc->ptr = current;
+    cc->count += len;
 }
 
-void sha256_final(SHA256_CTX *ctx, BYTE hash[])
+void
+sph_sha256_close(sph_sha256_context *cc, void *dst)
 {
-	WORD i;
+    unsigned char *buf;
+    size_t ptr;
+    sph_u64 bb;
 
-	i = ctx->datalen;
+    buf = cc->buf;
+    ptr = cc->ptr;
+    bb = cc->count << 3;
+    buf[ptr++] = 0x80;
+    if (ptr > 56) {
+        memset(buf + ptr, 0, 64 - ptr);
+        sha256_round((sph_u32 *)buf, cc->state);
+        memset(buf, 0, 56);
+    } else {
+        memset(buf + ptr, 0, 56 - ptr);
+    }
+    sph_enc64be(buf + 56, bb);
+    sha256_round((sph_u32 *)buf, cc->state);
+    sph_enc32be((unsigned char *)dst + 0, cc->state[0]);
+    sph_enc32be((unsigned char *)dst + 4, cc->state[1]);
+    sph_enc32be((unsigned char *)dst + 8, cc->state[2]);
+    sph_enc32be((unsigned char *)dst + 12, cc->state[3]);
+    sph_enc32be((unsigned char *)dst + 16, cc->state[4]);
+    sph_enc32be((unsigned char *)dst + 20, cc->state[5]);
+    sph_enc32be((unsigned char *)dst + 24, cc->state[6]);
+    sph_enc32be((unsigned char *)dst + 28, cc->state[7]);
+}
 
-	// Pad whatever data is left in the buffer.
-	if (ctx->datalen < 56) {
-		ctx->data[i++] = 0x80;
-		while (i < 56)
-			ctx->data[i++] = 0x00;
-	}
-	else {
-		ctx->data[i++] = 0x80;
-		while (i < 64)
-			ctx->data[i++] = 0x00;
-		sha256_transform(ctx, ctx->data);
-		memset(ctx->data, 0, 56);
-	}
+void
+sph_sha256_addbits_and_close(sph_sha256_context *cc, unsigned ub, unsigned n, void *dst)
+{
+    unsigned char *buf;
+    size_t ptr;
+    sph_u64 bb;
 
-	// Append to the padding the total message's length in bits and transform.
-	ctx->bitlen += ctx->datalen * 8;
-	ctx->data[63] = ctx->bitlen;
-	ctx->data[62] = ctx->bitlen >> 8;
-	ctx->data[61] = ctx->bitlen >> 16;
-	ctx->data[60] = ctx->bitlen >> 24;
-	ctx->data[59] = ctx->bitlen >> 32;
-	ctx->data[58] = ctx->bitlen >> 40;
-	ctx->data[57] = ctx->bitlen >> 48;
-	ctx->data[56] = ctx->bitlen >> 56;
-	sha256_transform(ctx, ctx->data);
+    buf = cc->buf;
+    ptr = cc->ptr;
+    bb = cc->count << 3;
+    bb += n;
+    if (n > 0) {
+        unsigned z;
 
-	// Since this implementation uses little endian byte ordering and SHA uses big endian,
-	// reverse all the bytes when copying the final state to the output hash.
-	for (i = 0; i < 4; ++i) {
-		hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
-		hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
-	}
+        buf[ptr ++] = ((ub & 0xFF) << (8 - n)) & 0xFF;
+        if (ptr == 64) {
+            sha256_round((sph_u32 *)buf, cc->state);
+            ptr = 0;
+        }
+        z = 0x80 >> n;
+        buf[ptr ++] = z;
+    } else {
+        buf[ptr ++] = 0x80;
+    }
+    if (ptr > 56) {
+        memset(buf + ptr, 0, 64 - ptr);
+        sha256_round((sph_u32 *)buf, cc->state);
+        memset(buf, 0, 56);
+    } else {
+        memset(buf + ptr, 0, 56 - ptr);
+    }
+    sph_enc64be(buf + 56, bb);
+    sha256_round((sph_u32 *)buf, cc->state);
+    sph_enc32be((unsigned char *)dst + 0, cc->state[0]);
+    sph_enc32be((unsigned char *)dst + 4, cc->state[1]);
+    sph_enc32be((unsigned char *)dst + 8, cc->state[2]);
+    sph_enc32be((unsigned char *)dst + 12, cc->state[3]);
+    sph_enc32be((unsigned char *)dst + 16, cc->state[4]);
+    sph_enc32be((unsigned char *)dst + 20, cc->state[5]);
+    sph_enc32be((unsigned char *)dst + 24, cc->state[6]);
+    sph_enc32be((unsigned char *)dst + 28, cc->state[7]);
+}
+
+void
+sph_sha256_comp(const sph_u32 msg[16], sph_u32 val[8])
+{
+    sha256_round(msg, val);
 }
