e7d24e328804277a1336b60dabe77ea1b10f85e9
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-20 18:14:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ meme_hash_module = Extension('meme_hash',
                              sources=['mememodule.c',
                                       'memehash.c',
                                       'sha256.c',
+                                      'sha3/md_helper.c'
                                       'sha3/blake.c',
                                       'sha3/bmw.c',
                                       'sha3/groestl.c',
