18ce93c75243a1c522b6663d86e303a5b13a79d8
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-20 14:04:11 +0800

    Update memehash.c

--- memehash.c
+++ memehash.c
@@ -1,10 +1,6 @@
 #include "memehash.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
-
+#include <string.h>
 #include "sha3/sph_blake.h"
 #include "sha3/sph_cubehash.h"
 #include "sha3/sph_shavite.h"
@@ -12,55 +8,58 @@
 #include "sha3/sph_echo.h"
 #include "sha3/sph_sha2.h"
 
-void meme_hash(const char* input, char* output, uint32_t len)
-{
+void meme_hash(const char* input, char* output, uint32_t len) {
+    // Define contexts for each hash algorithm
     sph_blake512_context     ctx_blake;
-    sph_cubehash512_context  ctx_cubehash1;
-    sph_shavite512_context   ctx_shavite1;
-    sph_simd512_context      ctx_simd1;
-    sph_echo512_context      ctx_echo1;
-    sph_sha256_context       ctx_sha;
+    sph_cubehash512_context  ctx_cubehash;
+    sph_shavite512_context   ctx_shavite;
+    sph_simd512_context      ctx_simd;
+    sph_echo512_context      ctx_echo;
+    sph_sha256_context       ctx_sha256;
 
-    uint32_t hash[16];
-    uint32_t hashA[16];
+    uint32_t hash[16];   // Intermediate hash result
+    uint32_t hashA[16];  // Final hash result
 
+    // Blake hash computation
     sph_blake512_init(&ctx_blake);
-    sph_blake512 (&ctx_blake, input, len);
-    sph_blake512_close (&ctx_blake, hash);
-	
-    sph_simd512_init (&ctx_simd1);
-    sph_simd512 (&ctx_simd1, hash, 64);
-    sph_simd512_close(&ctx_simd1, hash);
+    sph_blake512(&ctx_blake, input, len);
+    sph_blake512_close(&ctx_blake, hash);
 
-    sph_echo512_init (&ctx_echo1);
-    sph_echo512 (&ctx_echo1, hash, 64);
-    sph_echo512_close(&ctx_echo1, hash);
+    // SIMD hash computation
+    sph_simd512_init(&ctx_simd);
+    sph_simd512(&ctx_simd, hash, 64);
+    sph_simd512_close(&ctx_simd, hash);
 
-    sph_cubehash512_init (&ctx_cubehash1);
-    sph_cubehash512 (&ctx_cubehash1, hash, 64);
-    sph_cubehash512_close(&ctx_cubehash1, hash);
+    // Echo hash computation
+    sph_echo512_init(&ctx_echo);
+    sph_echo512(&ctx_echo, hash, 64);
+    sph_echo512_close(&ctx_echo, hash);
 
-    sph_shavite512_init (&ctx_shavite1);
-    sph_shavite512 (&ctx_shavite1, hash, 64);
-    sph_shavite512_close(&ctx_shavite1, hash);
+    // Cubehash hash computation
+    sph_cubehash512_init(&ctx_cubehash);
+    sph_cubehash512(&ctx_cubehash, hash, 64);
+    sph_cubehash512_close(&ctx_cubehash, hash);
 
-    sph_sha256_init(&ctx_sha);
-    sph_sha256 (&ctx_sha, hash, 64);
-    sph_sha256_close(&ctx_sha, hashA);
+    // Shavite hash computation
+    sph_shavite512_init(&ctx_shavite);
+    sph_shavite512(&ctx_shavite, hash, 64);
+    sph_shavite512_close(&ctx_shavite, hash);
 
-    for (int i=8;i<16;i++)
-        hashA[i]=0;
-	
-    sph_sha256_init(&ctx_sha);
-    sph_sha256 (&ctx_sha, hashA, 64);
-    sph_sha256_close(&ctx_sha, hashA);
+    // First SHA256 hash computation
+    sph_sha256_init(&ctx_sha256);
+    sph_sha256(&ctx_sha256, hash, 64);
+    sph_sha256_close(&ctx_sha256, hashA);
 
-    for (int i=8;i<16;i++)
-        hashA[i]=0;	
+    // Second SHA256 hash computation
+    sph_sha256_init(&ctx_sha256);
+    sph_sha256(&ctx_sha256, hashA, 64);
+    sph_sha256_close(&ctx_sha256, hashA);
 
-    sph_sha256_init(&ctx_sha);
-    sph_sha256 (&ctx_sha, hashA, 64);
-    sph_sha256_close(&ctx_sha, hash);	
+    // Third SHA256 hash computation
+    sph_sha256_init(&ctx_sha256);
+    sph_sha256(&ctx_sha256, hashA, 64);
+    sph_sha256_close(&ctx_sha256, hash);
 
+    // Copy the final hash result to the output buffer
     memcpy(output, hash, 32);
 }
