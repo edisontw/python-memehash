6331590a8a09eb9712659703d0c63b24fd35bb0c
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-23 23:48:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,23 @@ memehash_module = Extension('meme_hash',
         'sha3/sha256.c'
     ],
     include_dirs = ['.', './sha3'],
-    extra_compile_args = ['-O2', '-funroll-loops', '-fomit-frame-pointer', '-DSPH_SMALL_FOOTPRINT=1'],
-    define_macros = [('SPH_SMALL_FOOTPRINT', '1'), ('SPH_COMPACT_BLAKE', '1')]
+    extra_compile_args = [
+        '-O2', 
+        '-funroll-loops', 
+        '-fomit-frame-pointer',
+        '-DSPH_SMALL_FOOTPRINT=1',
+        '-fPIC'  # Ensure position-independent code
+    ],
+    extra_link_args = [
+        '-Wl,--export-dynamic'  # Export all symbols for dynamic linking
+    ],
+    define_macros = [
+        ('SPH_SMALL_FOOTPRINT', '1'),
+        ('SPH_COMPACT_BLAKE', '1'),
+        ('SPH_KECCAK_UNROLL', '0'),
+        ('SPH_SMALL_FOOTPRINT_SIMD', '1'),
+        ('SPH_SMALL_FOOTPRINT_ECHO', '1')
+    ]
 )
 
 setup (
@@ -21,7 +36,7 @@ setup (
     version = '1.0',
     description = 'Bindings for Memehash proof of work function',
     author = 'Edison Huang',
+    author_email = 'your.email@example.com',
     url = 'https://github.com/edisontw/python-memehash',
-    ext_modules = [memehash_module],
-    py_modules = ['meme_hash'],
+    ext_modules = [memehash_module]
 )
