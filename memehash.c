7a974db04dabbda6bd23d03f483f4378f1807862
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-10-29 00:28:22 +0800

    Update memehash.c

--- memehash.c
+++ memehash.c
@@ -6,7 +6,7 @@
 #include "sha3/sph_shavite.h"
 #include "sha3/sph_simd.h"
 #include "sha3/sph_echo.h"
-#include "sha3/sph_sha2.h"
+#include "sha3/sha2.h"
 
 void meme_hash(const char* input, char* output, uint32_t len) {
     sph_blake512_context     ctx_blake;
