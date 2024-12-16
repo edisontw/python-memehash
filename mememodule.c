5168b1ea1819d694219713ce5eebf4fd1094dc13
Edison Huang <107197406+edisontw@users.noreply.github.com>
2024-12-16 22:13:59 +0800

    Update mememodule.c

--- mememodule.c
+++ mememodule.c
@@ -2,24 +2,19 @@
 #include <Python.h>
 #include <stdint.h>
 #include <string.h>
-
 #include "memehash.h"
 
-// 反轉 32 bytes (prev_block_hash, merkle_root)
-static void reverse32(unsigned char* data)
-{
+// 反轉 32 bytes
+static void reverse32(unsigned char* data) {
     for (int i = 0; i < 16; i++) {
         unsigned char tmp = data[i];
         data[i] = data[31 - i];
         data[31 - i] = tmp;
     }
 }
-
-// 反轉 4 bytes (bits, nonce)
-static void reverse4(unsigned char* data)
-{
-    unsigned char tmp0 = data[0];
-    unsigned char tmp1 = data[1];
+// 反轉 4 bytes
+static void reverse4(unsigned char* data) {
+    unsigned char tmp0 = data[0], tmp1 = data[1];
     data[0] = data[3];
     data[1] = data[2];
     data[2] = tmp1;
@@ -31,7 +26,7 @@ static PyObject* meme_getpowhash(PyObject* self, PyObject* args)
     const unsigned char* block_header = NULL;
     Py_ssize_t header_len = 0;
 
-    // 解析 Python 傳入的 block header (必須為 80 bytes)
+    // 1) 解析 python 傳入的 80 bytes
     if (!PyArg_ParseTuple(args, "s#", &block_header, &header_len)) {
         return NULL;
     }
@@ -40,33 +35,27 @@ static PyObject* meme_getpowhash(PyObject* self, PyObject* args)
         return NULL;
     }
 
-    // 複製一份 header，我們要在 C 端做 byte-wise flip
+    // 2) 複製一份，做欄位反轉
     unsigned char real_header[80];
     memcpy(real_header, block_header, 80);
 
-    // 布局: version(4), prevhash(32), merkleroot(32), time(4), bits(4), nonce(4)
-    // 下標:
-    // 0..3: version
-    // 4..35: prevhash
-    // 36..67: merkleroot
-    // 68..71: time
-    // 72..75: bits
-    // 76..79: nonce
-
-    // 反轉 prevhash
+    // ============ 關鍵欄位翻轉 ============
+    // layout: ver(4), prevhash(32), merkle(32), time(4), bits(4), nonce(4)
+    // prevhash: offset = 4
     reverse32(real_header + 4);
-    // 反轉 merkle
+    // merkle: offset = 36
     reverse32(real_header + 36);
-    // 反轉 bits
+    // bits: offset = 72
     reverse4(real_header + 72);
-    // 反轉 nonce
+    // nonce: offset = 76
     reverse4(real_header + 76);
 
-    // 呼叫 meme_hash
+    // 3) 呼叫 meme_hash()
     unsigned char output[32];
-    memset(output, 0, 32);
+    memset(output, 0, sizeof(output));
     meme_hash((const char*)real_header, (char*)output, 80);
 
+    // 4) 回傳 Python bytes
 #if PY_MAJOR_VERSION >= 3
     return Py_BuildValue("y#", output, 32);
 #else
@@ -75,13 +64,8 @@ static PyObject* meme_getpowhash(PyObject* self, PyObject* args)
 }
 
 static PyMethodDef MemeMethods[] = {
-    {
-        "getPoWHash",
-        meme_getpowhash,
-        METH_VARARGS,
-        "Returns the proof of work hash using meme hash"
-    },
-    { NULL, NULL, 0, NULL }
+    {"getPoWHash", meme_getpowhash, METH_VARARGS, "Returns the PoW hash using meme hash"},
+    {NULL, NULL, 0, NULL}
 };
 
 #if PY_MAJOR_VERSION >= 3
@@ -93,13 +77,11 @@ static struct PyModuleDef MemeModule = {
     MemeMethods
 };
 
-PyMODINIT_FUNC PyInit_meme_hash(void)
-{
+PyMODINIT_FUNC PyInit_meme_hash(void) {
     return PyModule_Create(&MemeModule);
 }
 #else
-PyMODINIT_FUNC initmeme_hash(void)
-{
+PyMODINIT_FUNC initmeme_hash(void) {
     Py_InitModule("meme_hash", MemeMethods);
 }
 #endif
